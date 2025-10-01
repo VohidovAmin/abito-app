463d29670f4cae2f034ea58f744bb9a64dea364d
VohidovAmin <vohidovaminjon9@gmail.com>
2025-10-01 22:04:44 +0300

    Первый коммит React-проекта

---
+++ src/constans.js
@@ -0,0 +1,55 @@
+ export const cardArray = [
+    {
+        id: 0,
+        title: 'Первый товар',
+        price: '170 р',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    },
+
+    {
+        id: 1,
+        title: 'Второй товар',
+        price: '171 р',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    },
+
+    {
+        id: 2,
+        title: 'Третий товар',
+        price: '172 р',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    },
+
+    {
+        id: 3,
+        title: 'Четвертый товар',
+        price: '173 р',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    },
+
+    {
+        id: 4,
+        title: 'Пятый товар',
+        price: '174 р',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    },
+
+    {
+        id: 5,
+        title: 'Шестой товар',
+        price: '175 р',
+        address: 'Казань, р-н Вахитовский',
+        date: '10 июля 11:39',
+        img: '/images/card-img.png'
+    }
+]
